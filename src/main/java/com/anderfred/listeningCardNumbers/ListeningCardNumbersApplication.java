b70de41edb9cc74419e0bc5cf0a207eb6d165a10
Egor Belkov <Subgorn@gmail.com>
2019-06-29 20:49:50 +0700

    second

---
+++ src/main/java/com/anderfred/listeningCardNumbers/ListeningCardNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.anderfred.listeningCardNumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeningCardNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeningCardNumbersApplication.class, args);
+	}
+
+}
